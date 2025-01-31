a3466c451c7f3fe42a29f9de777d8f1d07f6b48d
Cash0017 <emifubuknho@gmail.com>
2025-01-31 15:19:34 +0000

    commit

--- supertrunfo.c
+++ supertrunfo.c
@@ -3,10 +3,16 @@ int main(){
 
 char codigo[5];
 char nome[100];
-int populacao;
+float populacao;
 float area;
 float pib;
 int pontosturisticos;
+float pibpercapita;
+float densidade;
+
+
+
+
 
 printf("cadastro de cidade\n");
 
@@ -17,7 +23,7 @@ printf("digite o nome da cidade: ");
 scanf("%s", nome);
 
 printf("digite a populacao: ");
-scanf("%d", &populacao);
+scanf("%f", &populacao);
 
 printf("digite a area em km: ");
 scanf("%f", &area);
@@ -25,16 +31,23 @@ scanf("%f", &area);
 printf("digite o pib: ");
 scanf("%f", &pib);
 
-printf("digite o numero de pontos turisticos: ");
+printf("digite o numero de pontos turisticos: \n");
 scanf("%d", &pontosturisticos);
+
+pibpercapita = pib / populacao;
+densidade = populacao / area;
 
 printf("Dados cadastrados da cidade:\n");
 printf("codigo da cidade: %s\n", codigo);
 printf("nome da cidade: %s\n", nome);
-printf("populacao da cidade: %d\n", populacao);
+printf("populacao da cidade: %.3f\n", populacao);
 printf("area de cidade: %f km\n", area);
 printf("pib da cidade: %.2f\n", pib);
 printf("numero de pontos turisticos: %d\n", pontosturisticos);
+printf("pib Per Capita: %.2f\n", pibpercapita);
+printf("densidade: %.2f\n", densidade);
+
+
 
 
 return 0;
